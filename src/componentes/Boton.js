8ccea343d3f05300b08b61c2871473d5a370b046
everardo277 <rene.contrerasari@nube.unadmexico.mx>
2022-09-26 20:52:30 -0500

    commit inicio

---
+++ src/componentes/Boton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../holas-de-estilo/Boton.css';
+
+function Boton (props){
+
+ const esOperador = valor => {
+
+  return isNaN(valor) && (valor !== '.') && (valor !== '=');
+
+ };
+
+    return (
+
+      <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null }`.trimEnd()} onClick={() => props.manejarClic(props.children)}>
+
+        {props.children}
+      </div>
+    );
+
+
+}
+
+export default Boton;
---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../holas-de-estilo/Pantalla.css';
+
+const Pantalla = ({input}) => (
+
+    <div className='input'>
+
+        {input}
+
+    </div>
+
+);
+
+export default Pantalla;
